eae63d22ff5caf16a3d642685ed567a4d92a9703
Machffud Tra Harana <machffud.thv@gmail.com>
2022-10-22 22:01:54 +0700

    add rename multiple file

---
+++ rename.py
@@ -0,0 +1,25 @@
+# Python 3 code to rename multiple
+# files in a directory or folder
+
+# importing os module
+import os
+
+# Function to rename multiple files
+def main():
+
+    list_folder = ["batak", "jawa", "kalimantan","minang","papua"]
+    for folder in list_folder:
+        for count, filename in enumerate(os.listdir(folder)):
+            dst = f"{folder}_{str(count+1)}.jpg"  
+            src =f"{folder}/{filename}" # foldername/filename, if .py file is outside folder
+            dst =f"{folder}/{dst}"
+            
+            # rename() function will
+            # rename all the files
+            os.rename(src, dst)
+
+# Driver Code
+if __name__ == '__main__':
+	
+	# Calling main() function
+	main()
